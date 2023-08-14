c05de9dd23a0121ad288dc6d098046bee1853efd
Joost Mulders <47423064+joostm1@users.noreply.github.com>
2023-08-14 12:24:13 +0200

    Yes

--- ATVKP/dbo/Tables/t-pompen.sql
+++ ATVKP/dbo/Tables/t-pompen.sql
@@ -1,9 +1,20 @@
 CREATE TABLE [dbo].[t-pompen] (
     [tuinadres]  VARCHAR (50) NULL,
-    [typefk]     VARCHAR (50) NULL,
+    ALTER TABLE [dbo].[t-pompen]
+    ADD [typefk] INT NULL;,
     [datumstart] DATETIME     NULL,
     [datumeind]  DATETIME     NULL,
     [status]     VARCHAR (50) NULL
 );
+GO
+
+
+ALTER TABLE [dbo].[t-pompen]
+    ADD CONSTRAINT [FK_t-pomptype] FOREIGN KEY ([typefk]) REFERENCES [dbo].[t-pomptype] ([pomptypepk]);
+GO
+
+
+ALTER TABLE [dbo].[t-pompen]
+    ADD CONSTRAINT [PK_t-pompen] PRIMARY KEY CLUSTERED ([pompid] ASC);
 GO
 
